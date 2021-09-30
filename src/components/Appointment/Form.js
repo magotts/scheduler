2b8c368662e3c30ca8b5fe467e8a97b07827b4eb
magotts <techybet@gmail.com>
2021-09-29 23:25:55 -0400

    adding save function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -17,6 +17,11 @@ export default function Form(props) {
     reset();
     props.onCancel();
   }
+
+  const save = () => {
+    props.onSave(name, interviewer);
+  }
+  
   return (
 <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
@@ -38,7 +43,7 @@ export default function Form(props) {
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button danger onClick={cancel} >Cancel</Button>
-      <Button confirm onClick={props.onSave}>Save</Button>
+      <Button confirm onClick={save}>Save</Button>
     </section>
   </section>
 </main>
