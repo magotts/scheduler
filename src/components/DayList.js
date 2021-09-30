bb47a3dadfde937adee645422eefdc863192cd9d
magotts <techybet@gmail.com>
2021-09-29 20:24:38 -0400

    modifying file to show correct output

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,13 +4,16 @@ import DayListItem from "components/DayListItem";
 export default function DayList(props) {  
 
   return props.days.map(day => 
-    <DayListItem 
+    <ul>
+      <DayListItem 
       id={day.id}
       name={day.name} 
       spots={day.spots} 
       selected={day.name === props.day}
       setDay={props.setDay}  
     />
+    </ul>
   );
   
 };
+
