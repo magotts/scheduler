e2587b9f7ab987b0855a08af4e6ffaba0a938e03
magotts <techybet@gmail.com>
2021-10-05 20:22:26 -0400

    remove unwanted lines

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,10 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
+import Confirm from "./Confirm";
+import Error from "./Error";
+
 import useVisualMode from "hooks/useVisualMode";
 
 
@@ -11,29 +15,79 @@ export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
-
+  const SAVING = "SAVING";
+  const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  // SAVE an Appointment
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch(error => {
+        transition(ERROR_SAVE, true)
+      })
+  }
+
+  // DELETE an Appointment
+  const destroy = () => {
+    transition(DELETING, true)
+    props.cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch(error => {
+        transition(ERROR_DELETE, true)
+  });
+}
+  // CONFIRM Delete
+  const deleteConfirm = () => {
+    transition(CONFIRM)
+  };
+
+  // EDIT Appointment
+  const editAppointment = () => {
+    transition(EDIT);
+  };
+
+
   return (
     
     <article className="appointment">
       <Header time={props.time} />
-      {/* {props.interview ? <Show student={props.interview.student}
-          interviewer={props.interview.interviewer} /> : <Empty />} */}
-
-{/* Update the onAdd function we pass to our Empty component to transition to the CREATE mode when the user clicks the add appointment button, instead of logging to the console. */}
         {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-        {mode === CREATE && <Form interviewers={props.interviewers} onCancel={back}/>}
+        {mode === CREATE && <Form interviewers={props.interviewers} onCancel={back} onSave = {save}/>}
+        {mode === SAVING && <Status message="Saving" />}
+        {mode === DELETING && <Status message="Deleting" />}
+        {mode === CONFIRM && <Confirm message="Are you sure you would like to delete?" onConfirm={destroy} 
+        onCancel = {() => back()} />}
+        {mode === EDIT &&  <Form 
+          name={props.interview.student}
+          interviewer={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onCancel = {() => back()}
+          onSave = {save}
+         />}
         {mode === SHOW && (
           <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-  
-          />
-)}
+          onEdit={editAppointment}
+          onDelete={deleteConfirm}
+          />)}
+          {mode === ERROR_SAVE && (<Error message="Could not save appointment" onClose={() => back()} />)}
+          {mode === ERROR_DELETE && (<Error message="Could not cancel appointment" onClose={() => back()} />)}
+
+        
     </article>
     
   )
